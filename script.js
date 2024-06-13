e6e0451a139e54dc17ae883ab801cbb452cf8dfd
MarchenkoAlexander <alexander.marchenko.222@gmail.com>
2024-06-13 13:48:29 +0200

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+const imgBox = document.querySelector("#imgBox")
+const qrImage = document.querySelector("#qrImage");
+const qrText = document.querySelector("#qrText");
+const generateBtn = document.querySelector("#generateBtn");
+
+const generateQR = () => {
+    if (qrText.value.length > 0) {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show-img");
+    } else {
+        qrText.classList.add("error");
+        setTimeout(() => {
+            qrText.classList.remove("error");
+        }, 1000)
+    }
+}
+
+generateBtn.addEventListener("click", generateQR);
